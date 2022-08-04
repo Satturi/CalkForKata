5077965e5b19026057e7d7a6b3fdbb30990cb4bc
Roman M.E <110542682+Satturi@users.noreply.github.com>
2022-08-04 22:38:05 +0500

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -3,25 +3,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-
+//
 public class Main1 {
     public static void main(String[] args) {
+        System.out.println(calc(""));
 
-        System.out.println("\n///   ОБЯЗАТЕЛЬНО К ПРОЧТЕНИЮ!!!   ///\n\n" +
-                "Условия использования программы.\n\n"+
-                "1\t-\tПрограмма выполняет операции + - * / с двумя числами;\n"+
-                "2\t-\tПрограмма работает единовременно только c одним видом систем исчисленпия" +
-                "\n\t\t* и только с целыми числами в них от 1 до 10 или с аналагом их в римской системе исчисления" +
-                "\n\t\t* программа принемает римские числа только прописанные в верхнем регистре;\n" +
-                "3\t-\tЗапись мат.выражения начинается с первого члена мат.выражения, " +
-                  "\n\t\tдалее через пробел указывается желаемая проводимая над ним операция, " +
-                  "\n\t\tдалее - пробел и указывается последий член мат.выражения;\n" +
-                "\n" +
-                "Введи мат.выражение:");
+    }
+    public static String calc(String input) {
 
-        String [] romanNumerals = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};                 //*массив для последуещей поверики на принадлежность чилел к системе исчесления
+        System.out.println("""
+
+                ///   ОБЯЗАТЕЛЬНО К ПРОЧТЕНИЮ!!!   ///
+
+                Условия использования программы.
+
+                1\t-\tПрограмма выполняет операции + - * / с двумя числами;
+                2\t-\tПрограмма работает единовременно только c одним видом систем исчисленпия
+                \t\t* и только с целыми числами в них от 1 до 10 или с аналагом их в римской системе исчисления
+                \t\t* программа принимает римские числа только прописанные в верхнем регистре;
+                3\t-\tЗапись мат.выражения начинается с первого члена мат.выражения,\s
+                \t\tдалее через пробел указывается желаемая проводимая над ним операция,\s
+                \t\tдалее - пробел и указывается последий член мат.выражения;
+
+                Введи мат.выражение:""");
+
+        String[] romanNumerals = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};                 //*массив для последуещей поверики на принадлежность чилел к системе исчесления
         List<String> romanList = new ArrayList<>(Arrays.asList(romanNumerals));                         //*инструмент для этого
-        String [] arabNumerals = {"1","2","3","4","5","6","7","8","9","10"};
+        String[] arabNumerals = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
         List<String> arabList = new ArrayList<>(Arrays.asList(arabNumerals));
 
         Scanner in = new Scanner(System.in);
@@ -29,10 +37,10 @@ public class Main1 {
         String[] str1_s = str1.split(" ", 3);
 
 
-
-
-
         Roman[] valuesRoman = Roman.values();
+
+        int otvet = 1000;
+        String otvetfin = "Не соблюдено условие программы!\tПрограмма завершена.";
 
         ConvertRomanToArab convertfirst = new ConvertRomanToArab();              //**создание обьекта ConvertRomanToArab для конвертации присвоенных в дальнейшем к ним чисел из рим в араб -> в int
         ConvertRomanToArab convertsecond = new ConvertRomanToArab();
@@ -45,7 +53,7 @@ public class Main1 {
             int firstNumber = Integer.parseInt(convertfirst.first1Number);        //**непосредствено конвертация араб в int
             String operation = str1_s[1];
             int secondNumber = Integer.parseInt(convertsecond.first1Number);
-            int otvet = 1000;
+            //int otvet = 1000;
 
             if (firstNumber < 1 || secondNumber < 1 || firstNumber > 10 || secondNumber > 10) {
                 System.out.println("Не соблюдено условие программы!\tПрограмма завершена.");
@@ -58,24 +66,33 @@ public class Main1 {
                 case "-" -> otvet = firstNumber - secondNumber;
                 case "*" -> otvet = firstNumber * secondNumber;
                 case "/" -> otvet = firstNumber / secondNumber;
-                default -> {System.out.println("Не соблюдено условие программы!\tПрограмма завершена.");System.exit(0);}
+                default -> {
+                    System.out.println("Не соблюдено условие программы!\tПрограмма завершена.");
+                    System.exit(0);
+                }
             }
 
 
-            if (romanList.contains(str1_s[0])&&romanList.contains(str1_s[2])){                                //*непосредственно проверка на принадлежность
-                System.out.println("Ответ: " + valuesRoman[otvet - 1]);}
-            else if (arabList.contains(str1_s[0])&&arabList.contains(str1_s[2]))
-                System.out.println("Ответ: " + otvet);
-            else {System.out.println("Не соблюдено условие программы!\tПрограмма завершена.");System.exit(0);}
+            //String otvetfin;
 
-        }catch (NumberFormatException | ArrayIndexOutOfBoundsException e){
+
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
             System.out.println("Не соблюдено условие программы!\tПрограмма завершена.");
         }
 
+        if (romanList.contains(str1_s[0]) && romanList.contains(str1_s[2])) {                                //*непосредственно проверка на принадлежность
+            //otvet -= 1;
+            otvetfin = String.valueOf(valuesRoman[otvet-1]);
+        } else if (arabList.contains(str1_s[0]) && arabList.contains(str1_s[2])) {
+            otvetfin = Integer.toString(otvet);
+        } else {
+            System.exit(0);
+        } //System.out.println("Не соблюдено условие программы!\tПрограмма завершена.");
 
-
+        return otvetfin;
     }
 }
+
 
 
 class ConvertRomanToArab {                                     //**конвертация рим.цифр в араб. для дальнейшей конвертации в int
